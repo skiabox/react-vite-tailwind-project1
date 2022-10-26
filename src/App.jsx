d2b84d7f5fe5efff7daabdbd1846a4eb102ad0c4
Stavros Kefaleas <administrator@Belkin.fritz.box>
2022-10-27 00:07:46 +0300

    Add existing project files to Git

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div className="bg-slate-900 w-full overflow-hidden">
+    <h1 className="text-violet-600">Hello, World!!</h1>
+  </div>
+);
+
+export default App;
